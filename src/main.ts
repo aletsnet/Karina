fccd38eff02e43b7aac3f8ca1b5b918ce1beae94
Alejandro Lopez Ortiz <Alejandro.bin@gmail.com>
2025-11-24 19:21:35 -0600

    primera pantalla

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./styles.css";
 import { invoke } from "@tauri-apps/api/core";
 
 let greetInputEl: HTMLInputElement | null;
